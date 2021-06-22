07147e6b4cf3fb2f13a90c75a0694f9fb3ffcb04
Ashley  Sands <ashley.sands01@gmail.com>
2021-06-22 10:49:50 -0500

    styles search results section

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 const searchResults = document.getElementById('search-results')
+const searchImages = document.getElementById('search-images')
 const form = document.getElementById('anime-search-form')
 
 const animeSearch = async () => {
   try {
     let animeShowName = document.getElementById('search-shows').value
     document.getElementById('search-shows').value = ''
-    const animeSearchURL = `https://api.jikan.moe/v3/search/anime?q=${animeShowName}`
+    const animeSearchURL = `https://api.jikan.moe/v3/search/anime?q=${animeShowName}&limit=4`
     const animeSearchResults = await axios.get(animeSearchURL)
     // console.log(animeSearchResults.data.results)
     const results = animeSearchResults.data.results
-
+    
+    searchResults.insertAdjacentHTML('afterbegin', "<h2>Search Results</h2>")
+    
     for (let i = 0; i < results.length; i++) {
       // console.log(results[i].image_url)
       console.log(results[i].title)
 
       //Display search result names
-      let animeTitle = results[i].title
-      animeTitleNames = document.createElement("h2")
-      animeTitleNames.textContent = animeTitle
-      searchResults.append(animeTitleNames)
+      // let animeTitle = results[i].title
+      // animeTitleNames = document.createElement("h2")
+      // animeTitleNames.textContent = animeTitle
+      // searchResults.append(animeTitleNames)
 
 
     //Display search result names images
+
       let animeImageResults = results[i].image_url
       animeImage = document.createElement("img")
       animeImage.setAttribute("src", animeImageResults)
-      searchResults.append(animeImage)
+      animeImage.classList.add('limited-results')
+      // searchImages.appendChild(animeImage)
+
+      searchImages.insertAdjacentElement('beforeend', animeImage)
     }
 
 
@@ -48,3 +55,4 @@ function removeSearchResults() {
   searchResults.removeChild(searchResults.lastChild)
   }
 }
+
